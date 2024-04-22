b740d4d2acdc83060471c2c3088f50b07e8a2ae2
Miguelitox36 <miguelitox36@hotmail.com>
2024-04-22 17:17:40 -0500

    Scripts

---
+++ Semana 4 - Actividad 1/Semana 4 - Actividad 1/GeneradorNumeros.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana_4___Actividad_1
+{
+    class GeneradorNumeros
+    {
+        public int Fibonacci(int n)
+        {
+            if (n <= 1)
+                return n;
+
+            int a = 0, b = 1;
+            for (int i = 2; i <= n; i++)
+            {
+                int temp = a + b;
+                a = b;
+                b = temp;
+            }
+            return b;
+        }
+
+        public int Primo(int n)
+        {
+            int count = 0;
+            int num = 2;
+            while (count < n)
+            {
+                bool esPrimo = true;
+                for (int i = 2; i <= Math.Sqrt(num); i++)
+                {
+                    if (num % i == 0)
+                    {
+                        esPrimo = false;
+                        break;
+                    }
+                }
+                if (esPrimo)
+                    count++;
+                num++;
+            }
+            return num - 1;
+        }
+
+        public int NumeroPerfecto(int n)
+        {
+            int count = 0;
+            int num = 2;
+            while (count < n)
+            {
+                int sum = 1;
+                for (int i = 2; i * i <= num; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        if (i * i != num)
+                            sum = sum + i + num / i;
+                        else
+                            sum = sum + i;
+                    }
+                }
+                if (sum == num)
+                    count++;
+                num++;
+            }
+            return num - 1;
+        }
+    }
+}
---
+++ Semana 4 - Actividad 1/Semana 4 - Actividad 1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {        
+        GeneradorNumeros generador = new GeneradorNumeros();
+
+        
+        while (true)
+        {
+            Console.WriteLine("Seleccione una opción:");
+            Console.WriteLine("1. Obtener el término n de Fibonacci");
+            Console.WriteLine("2. Obtener el primo número n");
+            Console.WriteLine("3. Obtener el número perfecto n");
+            Console.WriteLine("4. Salir");
+
+            int opcion = Convert.ToInt32(Console.ReadLine());
+
+            if (opcion == 4)
+            {
+                Console.WriteLine("¡Adiós!");
+                break;
+            }
+
+            Console.WriteLine("Ingrese el valor de n:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            switch (opcion)
+            {
+                case 1:
+                    Console.WriteLine($"El término {n} de Fibonacci es: {generador.Fibonacci(n)}");
+                    break;
+                case 2:
+                    Console.WriteLine($"El primo número {n} es: {generador.Primo(n)}");
+                    break;
+                case 3:
+                    Console.WriteLine($"El número perfecto número {n} es: {generador.NumeroPerfecto(n)}");
+                    break;
+                default:
+                    Console.WriteLine("Opción inválida. Inténtalo de nuevo.");
+                    break;
+            }
+        }
+    }
+}
